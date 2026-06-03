c475f0aaa2848a6403ffc5c1dc8a3ec90b14abb2
Iader E. Garcia G. <iadergg@gmail.com>
2026-06-03 01:22:17 -0500

    fix(memories): add missing ActivityIndicator import

--- src/presentation/components/home/MemoryCard.tsx
+++ src/presentation/components/home/MemoryCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Card, Text, Chip, useTheme } from 'react-native-paper';
+import { Card, Text, Chip, useTheme, ActivityIndicator } from 'react-native-paper';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Memory } from '../../../domain/memories/entities/Memory';
 
